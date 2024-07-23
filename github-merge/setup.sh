de12984f43402a96d074c8fc6c936d520f149819
David唐辉 <david921518@qq.com>
2024-07-23 14:21:35 +0800

    add scripts for github-merge

---
+++ github-merge/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git clone -b main git@github.com:david921518/dev-tools.git main
+git clone -b gitee git@github.com:david921518/dev-tools.git gitee
+git clone -b gitlab git@github.com:david921518/dev-tools.git gitlab
+

9dfd548218ad42a1497b5d0a037b00c409745e66
David唐辉 <david921518@qq.com>
2024-07-24 07:12:44 +0800

    add qkd-app in github-merge/

---
+++ github-merge/qkd-app/replace_gitlab.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# tested on Ubuntu 22.04
+
+github_url='https://github.com/david921518/qkd-app/blob/master'
+gitlab_url='https://gitlab.com/david921518/qkd-app/blob/gitlab'
+ignore_files=("./github-merge/setup.sh" "./github-merge/replace_gitee.sh" "./github-merge/replace_gitlab.sh" "./LICENSE")
+
+rm -rf ./main_tmp
+mkdir -p ./main_tmp
+
+rm -rf ./gitlab/*
+
+cp -af ./main/* ./main_tmp/
+cp -af ./main/* ./gitlab/
+
+cd ./main_tmp/
+files=$(find .)
+cd ../
+
+for filename in $files
+do
+	if [ -f ./main_tmp/$filename ]; then
+		echo "./main_tmp/$filename is regular file"
+		matched='false';
+		for elem in "${ignore_files[@]}"; do
+			if [[ "$elem" == "$filename" ]]; then
+				matched='true';
+			fi
+		done
+		if [[ "$matched" == 'false' ]]; then
+			echo "replace $filename"
+			sed "s^$github_url^$gitlab_url^" "./main_tmp/$filename" > "./gitlab/$filename"
+		else
+			echo "ignore $filename"
+		fi
+	else
+		echo "./main_tmp/$filename is not regular file"
+	fi
+done
+
+rm -rf ./main_tmp/
+
+# git commit to github
+cd ./gitlab/
+git add *
+git commit -a -m "merge with main branch"
+git push origin gitlab
+cd ../
---
+++ github-merge/qkd-app/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# tested on Ubuntu 22.04
+
+git clone -b main git@github.com:david921518/qkd-app.git main
+git clone -b gitee git@github.com:david921518/qkd-app.git gitee
+git clone -b gitlab git@github.com:david921518/qkd-app.git gitlab
+

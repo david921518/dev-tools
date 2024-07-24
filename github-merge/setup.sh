7126c07984ab9c97f5f1dbc6960572aa7f2086a8
David唐辉 <david921518@qq.com>
2024-07-24 14:17:12 +0800

    merge with main branch

--- github-merge/setup.sh
+++ github-merge/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# tested on Ubuntu 22.04
+
 git clone -b main git@github.com:david921518/dev-tools.git main
 git clone -b gitee git@github.com:david921518/dev-tools.git gitee
 git clone -b gitlab git@github.com:david921518/dev-tools.git gitlab
+git clone -b gitcode git@github.com:david921518/dev-tools.git gitcode
 

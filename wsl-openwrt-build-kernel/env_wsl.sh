5b52c819755cef6118615994f3ca031ec71b84e9
David唐辉 <david921518@qq.com>
2024-08-02 14:00:05 +0800

    add wsl-openwrt-build-kernel/ for building OpenWrt kernel on Windows/WSL environment
    Signed-off-by: David唐辉 <david921518@qq.com>

---
+++ wsl-openwrt-build-kernel/env_wsl.sh
@@ -0,0 +1,2 @@
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=/home/yuhui/wks/openwrt/openwrt-sdk-23.05.4-x86-64_gcc-12.3.0_musl.Linux-x86_64/staging_dir/toolchain-x86_64_gcc-12.3.0_musl/bin:$PATH
